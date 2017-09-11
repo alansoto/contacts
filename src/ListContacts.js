9253179704561b4dd4de1c6a5a12a3340d32f53a
alan <me@alansoto.info>
2017-09-11 22:03:26 +1000

    implement remove contact using state

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -14,8 +14,8 @@ function ListContacts(props){
               <p>{contact.name}</p>
               <p>{contact.email}</p>
             </div>
-
-            <button className='contact-remove'>
+            
+            <button className='contact-remove' onClick={()=> props.onDeleteContact(contact)}>
               Remove
             </button>
           </li>
